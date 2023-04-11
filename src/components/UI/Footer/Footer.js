075d088832c9978b4866cbeff300bfea4b558283
nolan-ayer-dev <nolan.ayer.dev@gmail.com>
2023-04-10 21:50:34 -0700

    adjusted footer for various screen sizes

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -33,14 +33,17 @@ const Footer = () => {
         </li>
       </ul>
       <article className={styles.footerCopywrite}>
-        I do not own the rights to any of the photographs displayed on this
-        site. This is just a pseudo-copywrite article informing you that this
-        site is very much copywrited even though its not.
-        <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        <section>
+          I do not own the rights to any of the photographs displayed on this
+          site. This is just a pseudo-copywrite article informing you that this
+          site is very much copywrited even though its not.
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </section>
       </article>
     </section>
   );
